f9fa6b19e9de2d46dcb9ce084164bffd578abaa3
souzarix <lucas.lin@efrei.net>
2026-04-06 13:13:41 +0200

    Update niveau_menu.py

--- Code/Menu/niveau_menu.py
+++ Code/Menu/niveau_menu.py
@@ -1,43 +1,14 @@
+from Code.Utils.classes import*
 import pygame
-from Code.Utils.classes import *
 
-police = "Arial"
-
-
-def init_main_menu(police):
-    # Use Pygame's default font instead of SysFont to avoid system font scan issues.
-    # This is more reliable across machines and prevents crashes during startup.
-    title_font = pygame.font.Font(None, 48)
-    button_font = pygame.font.Font(None, 48)
-
+def init_lvl_menu(police):
     element = [
-        ButtonClass(
-            pygame.Rect(0, 0, 1280, 720),
-            "Asset/menu/main_menu_background.png",
-            title_font,
-            None,
-            (100, 100, 100),
-            0
-        ),
-        ButtonClass(
-            pygame.Rect(340, 130, 600, 120),
-            "Asset/menu/main_menu_txt.png",
-            title_font,
-            None,
-            (100, 100, 100),
-            0
-        ),
-        ButtonClass(
-            pygame.Rect(460, 360, 360, 80),
-            "Jouer",
-            button_font,
-            -1
-        ),
-        ButtonClass(
-            pygame.Rect(460, 470, 360, 80),
-            "Quitter",
-            button_font,
-            0
-        )
+        ButtonClass(pygame.Rect(0, 0, 1280, 720), "Asset/menu/niveau_menu_background.png", pygame.font.SysFont(police, 48), None, (100, 100, 100), 0),
+        ButtonClass(pygame.Rect(440, 150, 400, 100), "Asset/menu/niveau_menu_txt.png", pygame.font.SysFont(police, 48),None, (100, 100, 100), 0),
+        ButtonClass(pygame.Rect(195, 320, 200, 200), "1", pygame.font.SysFont(police, 200), 1),
+        ButtonClass(pygame.Rect(425, 320, 200, 200), "2", pygame.font.SysFont(police, 200), 2),
+        ButtonClass(pygame.Rect(655, 320, 200, 200), "3", pygame.font.SysFont(police, 200), 3),
+        ButtonClass(pygame.Rect(885, 320, 200, 200), "4", pygame.font.SysFont(police, 200), 4),
+        ButtonClass(pygame.Rect(1220, 10, 50, 50), "Asset/menu/parametre.png", pygame.font.SysFont(police, 200), -3, "Asset/menu/parametre_hover.png")
     ]
     return element
